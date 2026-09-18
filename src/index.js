653ff362771580ca861f68450e3fcdefb4b889c1
Samarth <samarthgoss@gmail.com>
2026-09-18 21:57:39 +0530

    custom api response and error handling

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 
  import mongoose from "mongoose";
  import express from "express";
- 
+
 ( async () => {
     try {
         await mongoose.connect(`${process.env.MONGODB_URI}`);
@@ -39,3 +39,11 @@ dotenv.config({
 });
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️   Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
